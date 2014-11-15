fe0850de122c52dce352205f37f2e2aef274bdb4
Ishita Shah <ishah@ishah-mn1.linkedin.biz>
2014-11-15 11:22:41 -0800

    content

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from scrape import main
 
 app = Flask(__name__)
@@ -10,10 +10,30 @@ def home():
 @app.route('/application')
 def application():
     print request
-    companies = request.args.get('companies')
+    #companies = request.args.get('companies')
+    companies = [
+        {
+            'company': 'ABC'
+        },
+        {
+            'company': 'XYZ'
+        }
+    ]
     print companies
-    blogs = main(companies)
-    return render_template('application.html')
+    #blogs = main(companies)
+    blogs = [
+        {
+            'title': 'ABC',
+            'tags': ['abc','xyz'],
+            'date': 'yyymmdd'
+        },
+        {
+            'title': 'ABC',
+            'tags': ['abc','xyz'],
+            'date': 'yyymmdd'
+        }
+    ]
+    return render_template('application.html', companies=companies, blogs=blogs)
 
 if __name__ == '__main__':
 	app.run(debug=True)
