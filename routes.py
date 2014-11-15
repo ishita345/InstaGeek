845233c9d7adbcbd4ff4eb21d3a8067e23205df6
Ishita Shah <ishah@ishah-mn1.linkedin.biz>
2014-11-15 04:04:28 -0800

    init

--- routes.py
+++ routes.py
@@ -4,31 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('old-profile.html')
+	return render_template('index.html')
 
-@app.route('/dwb')
-def dwb():
-    return render_template('dwb.html')
-
-@app.route('/dwb-donate')
-def dwbDonate():
-    return render_template('dwb-donate.html')
-
-@app.route('/updated-profile')
-def profileUpdated():
-    return render_template('profile-updated.html')
-
-@app.route('/authenticate')
-def authenticate():
-    return render_template('authenticate.html')
-
-@app.route('/oxfm')
-def oxfm():
-    return render_template('oxfam.html')
-
-@app.route('/thank-you')
-def thankyou():
-	return render_template('thank-you.html')
+@app.route('/appl')
+def appl():
+    return render_template('app.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
